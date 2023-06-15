cd65a45184f249284fcc3882bcff6b4d0ea083be
Suttirak <raksuttirak2@gmail.com>
2023-06-15 18:51:43 +0700

    Change base to javascript instead of typescript

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby SSG`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
